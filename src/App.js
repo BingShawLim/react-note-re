57ce5e27345079b9d2754ff3fa8c8cdc27dd25a2
BingShawLim <shaw.a.muvee@gmail.com>
2020-03-16 13:32:58 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
+import Flash from './components/Flash';
+import axios from 'axios';
+import urlFor from './helpers/urlFor';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false,
+      notes: [],
+      note: {},
+      newTag: false,
+      error: ''
+    };
+  }
+
+  toggleNote = () => {
+    this.setState({ 
+      showNote: !this.state.showNote ,
+      note: {}
+    });
+  }
+
+  getNotes = () => {
+    axios.get(urlFor('notes'))
+    .then((res) => this.setState({notes : res.data}))
+    .catch((err) => console.log(err.response.data));
+  }
+
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }))
+    .catch((err) => console.log(err.response.data));
+  }
+
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    }else{
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
+    .then((res) => this.setState({showNote: false}))
+    .catch((err) => {
+      const {errors} = err.response.data;
+      if (errors.content) {
+        this.setState({error: "Missing Note Content!"});
+      } else if (errors.title) {
+        this.setState({error: "Missing Note Title!"})
+      }
+    });
+  }
+
+  deleteNote = (id) => {
+    const newNotesState = this.state.notes.filter((note) => note.id !== id);
+    axios.delete(urlFor(`notes/${id}`))
+    .then((res) => this.setState({notes : newNotesState}))
+    .catch((err) => console.log(err.response.data));
+  }
+
+  showTagForm = () => {
+    this.setState({ newTag: true});
+  }
+
+  closeTagForm = () => {
+    this.setState({newTag: false});
+  }
+
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tags`), data)
+    .then((res) => this.getNote(noteId))
+    .catch((err) => {
+      const {errors} = err.response.data;
+      if (errors.name) {
+        this.setState({ error: "Missing Tag Name!"});
+      }
+    });
+  }
+
+  deleteTag = (noteId, id) => {
+    axios.delete(urlFor(`/tags/${id}`))
+    .then((res) => this.getNote(noteId))
+    .catch((err) => console.log(err.response.data));
+  }
+
+  resetError = () => {
+    this.setState({error: ""});
+  }
+
+  render() {
+    const {showNote, notes, note, newTag, error} = this.state;
+
+    return (
+      <div className="App">
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        {error && <Flash error={error} resetError={this.resetError} />}
+        { showNote ? 
+        <Note
+        note = {note}
+        submitNote = {this.submitNote}
+        showTagForm = {this.showTagForm}
+        closeTagForm = {this.closeTagForm}
+        submitTag = {this.submitTag}
+        deleteTag = {this.deleteTag}
+        newTag = {newTag}
+         /> 
+        :
+        <List 
+        getNotes={this.getNotes} 
+        notes = {notes}
+        getNote = {this.getNote}
+        deleteNote = {this.deleteNote}
+         />
+         }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/helpers/urlFor.js
@@ -0,0 +1,5 @@
+const urlFor = (endpoint) => {
+    return 'https://note-api-shaw-lim.herokuapp.com/' +endpoint;
+}
+
+export default urlFor;
